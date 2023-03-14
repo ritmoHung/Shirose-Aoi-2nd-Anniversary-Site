ba15a608ecb9f797ed43f495127110a0146d834a
Ritmo <jajh102098@gmail.com>
2023-03-14 21:36:43 +0800

    Update: V0.3, Parallax effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,57 @@ window.addEventListener('load', emo);
 
 
 
+// Parallax Sizing
+const tachieSizing = () => {
+    var tachieContainers = document.querySelectorAll('.tachie-container');
+    var viewportRatio = vw(100) / vh(100);
+    var imageRatio = 15 / 9;
+
+    if(viewportRatio > imageRatio) {
+        var rescale = 0.98 * (viewportRatio / imageRatio);
+        for(var index = 0; index < tachieContainers.length; index++) {
+            tachieContainers[index].style.transform = "scale(" + rescale + ")";
+        }
+    }
+    else {
+        for(var index = 0; index < tachieContainers.length; index++) {
+            tachieContainers[index].style.transform = "scale(1)";
+        }
+    }
+}
+window.addEventListener('load', tachieSizing);
+window.addEventListener('resize', tachieSizing);
+
+
+
+// Parallax
+let mouse = {
+    x: 0,
+    y: 0,
+}
+function MousePosition() {
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;
+}
+
+const tachieParallax = () => {
+    MousePosition();
+    var tachieBGTop = document.getElementById('tachieBGTop');
+    var tachieBGBottom = document.getElementById('tachieBGBottom');
+    var tachie = document.getElementById('tachie');
+    var parallaxBreakpoint = vh(100);
+
+    if(window.scrollY < parallaxBreakpoint) {
+        var optDX = 0.01 * (mouse.x - vw(50));
+        tachieBGTop.style.transform = "translateX(" + -optDX + "px" + ")";
+        tachieBGBottom.style.transform = "translateX(" + 2 * optDX + "px" + ")";
+        tachie.style.transform = "translateX(" + 3 * optDX + "px" + ")";
+    }
+}
+window.addEventListener('mousemove', tachieParallax);
+
+
+
 // Arrow: Scroll Down Indicator
 const scrollDownArrowVis = () => {
     var arrow = document.getElementById('scrollDownArrow');
@@ -22,33 +73,3 @@ const scrollDownArrowVis = () => {
     }
 }
 window.addEventListener('scroll', scrollDownArrowVis);
-
-
-
-// Parallax
-let mouse = {
-    x: 0,
-    y: 0,
-}
-
-function MousePosition() {
-    mouse.x = event.pageX;
-    mouse.y = event.pageY;
-}
-
-const parallaxHeader = () => {
-    MousePosition();
-    var tachieBGTop = document.getElementById('tachieBGTop');
-    var tachieBGBottom = document.getElementById('tachieBGBottom');
-    var tachie = document.getElementById('tachie');
-    var parallaxBreakpoint = vh(100);
-
-    if(window.scrollY < parallaxBreakpoint) {
-        var dx = mouse.x - vw(50);
-        console.log(mouse.x, dx);
-        tachieBGTop.style.left = dx + "px";
-        tachieBGBottom.style.left = dx + "px";
-        tachie.style.left = dx + "px";
-    }
-}
-window.addEventListener('mousemove', parallaxHeader);
