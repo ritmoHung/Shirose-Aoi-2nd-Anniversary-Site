5d2ac8659e6f4e5845ad37c3051f8a5bcdf5656e
Ritmo <jajh102098@gmail.com>
2023-03-15 07:07:58 +0800

    Update: V0.4, Snowflake particles & tachie anim

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,3 +76,10 @@ const scrollDownArrowVis = () => {
     }
 }
 window.addEventListener('scroll', scrollDownArrowVis);
+
+
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './assets/js/particles-config.json', function() {
+    console.log('callback - particles.js config loaded');
+});
